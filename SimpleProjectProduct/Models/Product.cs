1159ae9fc8dd7b926222478ee221cfcd820b8ae8
artur.simon <artur_simon@hotmail.com>
2022-05-26 10:03:00 -0300

    non negative price

--- SimpleProjectProduct/Models/Product.cs
+++ SimpleProjectProduct/Models/Product.cs
@@ -6,9 +6,14 @@ using System.Threading.Tasks;
 namespace SimpleProjectProduct.Models;
 
 public class Product
-{      
+{
+    private double preco;
     public int Id { get; set; }
     public int Estoque { get; set; }
     public String Nome { get; set; }
-    public double Preco { get; set; }
+    public double Preco
+    {
+        get { return preco; }
+        set { preco = value > 0 ? value : 0; }
+    }
 }
